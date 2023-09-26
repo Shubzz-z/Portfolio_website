c3a2d1ecad8102b5478a9fd62d03267e81ee5800
Shubzz-z <smore2630@gmail.com>
2023-09-27 02:28:25 +0530

    home image tuchup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import men from '../assets/men.jpg'
+import men from '../assets/banner.svg'
+// import href from '.../public/resume/Shubham_Resume.pdf'
 import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
-import {Link} from 'react-scroll'
+// import {Link} from 'react-scroll'
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-        <div className=' max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center sm:mt-20'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white'>I am Full Stack Developer</h2>
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 md:mt-8'>
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row pimp'>
+            <div className='flex flex-col'>
+                <h2 className='text-4xl sm:text-5xl md:text-7xl font-bold text-white'>I am Full Stack Developer</h2>
                 <p className='text-gray-500 py-4 max-w-md'>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 150.
                 </p>
                 <div>
-                <Link to='projects' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                    Projects
+                <a href='/resume/Shubham_Resume.pdf' download target='_blank' className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                    Download CV
                     <span className='group-hover:rotate-90 duration-300 '>
                         <MdOutlineKeyboardArrowRight size={25} className=' ml-1'/>
                     </span>
-                </Link>
+                </a>
                 </div>
             </div>
-            <div>
-                <img src={men} alt="my Profile" className='rounded-2xl mx-auto w-2/3 md:w-full'/>
+            <div className='md:pb-7'>
+                {/* <img src={men} alt="my Profile" className='rounded-2xl mx-auto maximp m'/> */}
+                <img src={men} alt="my Profile" className='rounded-2xl mx-auto maximp'/>
             </div>
         </div>
     </div>
